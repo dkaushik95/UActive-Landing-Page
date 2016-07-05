6352e3be4ceacb2aed9d8b5c62b14c0f447105e8
Dishant Kaushik <dkaushik95@gmail.com>
2016-07-05 12:09:16 +0530

    Made the site smoother, added a screenshot

--- js/code.js
+++ js/code.js
@@ -28,20 +28,135 @@ $(document).ready(function(s) {
             	$("#sld_2").attr("src", "images/VideoPage/2.PNG"); 
             	$("#sld_3").attr("src", "images/VideoPage/3.PNG"); 
             	$("#sld_4").attr("src", "images/VideoPage/4.PNG");
-        	}
+                //section2
+                $(".pins").css("animation", ""), 
+                $(".pins").css("animation-iteration-count", ""), 
+                $('.pinss').css('animation',''),
+                $('.pinss').css('animation-iteration-count',''),
+                $('.cloud').css('animation',''),
+                $('.cloud').css('animation-iteration-count',''),
+                $('.cycle').css('animation',''),
+                $('.cycle').css('animation-iteration-count',''),
+                // section3
+                $('#head_top').css('animation',''),
+                $('#head_top').css('animation-iteration-count',''),
+                $('#head_bottom').css('animation',''),
+                $('#head_bottom').css('animation-iteration-count',''),
+                $('#head_left').css('animation',''),
+                $('#head_left').css('animation-iteration-count',''),
+                $('#head_right').css('animation',''),
+                $('#head_right').css('animation-iteration-count',''),
+
+                $('#section4 .image_block img:nth-child(n+2)').css('animation',''),
+                $('#section4 .image_block img:nth-child(n+2)').css('animation-iteration-count','');
+            }
             1 != t ? (
             	$(".icons_left").show(), 
-            	$("#section1").css("visibility", "hidden"), 
-            	$("#sld_1").attr("src", "images/VideoPage/1.PNG"), 
-            	$("#sld_2").attr("src", "images/VideoPage/2.PNG"), 
-            	$("#sld_3").attr("src", "images/VideoPage/3.PNG"), 
-            	$("#sld_4").attr("src", "images/VideoPage/4.PNG")) 
+            	$("#section1").css("visibility", "hidden"))
             : (
             	$(".icons_left").hide(), 
             	$("#section1").css("visibility", "visible")
     		)
     		, 
-    		2 == t ? ($(".icons_left").fadeIn(500), $(".pins").css("animation", "poptop ease-in-out 1.5s"), $(".pins").css("animation-iteration-count", "1"), $("#sld_1").attr("src", "images/PickUpPage/1.png"), $("#sld_2").attr("src", "images/PickUpPage/2.png"), $("#sld_3").attr("src", "images/PickUpPage/3.png"), $("#sld_4").attr("src", "images/PickUpPage/4.png")) : 3 == t ? ($("#sld_1").attr("src", "images/BuddyUpPage/1.png"), $("#sld_2").attr("src", "images/BuddyUpPage/2.png"), $("#sld_3").attr("src", "images/BuddyUpPage/3.png"), $("#sld_4").attr("src", "images/BuddyUpPage/2.png")) : 4 == t ? ($("#medal").css("animation", "fadeDown ease-in-out 0.5s"), $("#medal").css("animation-iteration-count", "1"), $("#sld_1").attr("src", "images/RewardsPage/1.png"), $("#sld_2").attr("src", "images/RewardsPage/2.png"), $("#sld_3").attr("src", "images/RewardsPage/1.png"), $("#sld_4").attr("src", "images/RewardsPage/2.png")) : ($(".pins").css("animation", ""), $(".pins").css("animation-iteration-count", ""), $("#medal").css("animation", ""), $("#medal").css("animation-iteration-count", ""))
+    		2 == t ? (
+                $(".icons_left").fadeIn(500),
+                $(".pins").css("animation", "poptop ease-in-out 1.5s"), 
+                $(".pins").css("animation-iteration-count", "1"),
+                $('.pinss').css('animation','animationFrames ease-in-out 14s'),
+                $('.pinss').css('animation-iteration-count','infinite'),
+                $('.cloud').css('animation','fadeout ease-in-out 5s'),
+                $('.cloud').css('animation-iteration-count','infinite'),
+                $('.cycle').css('animation','moveandcomeback ease-in-out 14s'),
+                $('.cycle').css('animation-iteration-count','infinite'), 
+                $("#sld_1").attr("src", "images/PickUpPage/1.png"), 
+                $("#sld_2").attr("src", "images/PickUpPage/2.png"), 
+                $("#sld_3").attr("src", "images/PickUpPage/3.png"), 
+                $("#sld_4").attr("src", "images/PickUpPage/4.png"),
+
+
+                $('#head_top').css('animation',''),
+                $('#head_top').css('animation-iteration-count',''),
+                $('#head_bottom').css('animation',''),
+                $('#head_bottom').css('animation-iteration-count',''),
+                $('#head_left').css('animation',''),
+                $('#head_left').css('animation-iteration-count',''),
+                $('#head_right').css('animation',''),
+                $('#head_right').css('animation-iteration-count',''),
+
+                $('#section4 .image_block img:nth-child(n+2)').css('animation',''),
+                $('#section4 .image_block img:nth-child(n+2)').css('animation-iteration-count','')
+            )
+            : 
+            3 == t ? (
+                $("#sld_1").attr("src", "images/BuddyUpPage/1.png"), 
+                $("#sld_2").attr("src", "images/BuddyUpPage/2.png"), 
+                $("#sld_3").attr("src", "images/BuddyUpPage/3.png"), 
+                $("#sld_4").attr("src", "images/BuddyUpPage/4.png"),
+                $('#head_top').css('animation','bottomanimation linear 4s'),
+                $('#head_top').css('animation-iteration-count','infinite'),
+                $('#head_bottom').css('animation','topanimation linear 6s'),
+                $('#head_bottom').css('animation-iteration-count','infinite'),
+                $('#head_left').css('animation','bottomanimation linear 4s'),
+                $('#head_left').css('animation-iteration-count','infinite'),
+                $('#head_right').css('animation','topanimation linear 6s'),
+                $('#head_right').css('animation-iteration-count','infinite'),
+
+                $(".pins").css("animation", ""), 
+                $(".pins").css("animation-iteration-count", ""), 
+                $('.pinss').css('animation',''),
+                $('.pinss').css('animation-iteration-count',''),
+                $('.cloud').css('animation',''),
+                $('.cloud').css('animation-iteration-count',''),
+                $('.cycle').css('animation',''),
+                $('.cycle').css('animation-iteration-count',''),
+
+                $('#section4 .image_block img:nth-child(n+2)').css('animation',''),
+                $('#section4 .image_block img:nth-child(n+2)').css('animation-iteration-count','')
+
+            ) 
+            : 
+            4 == t ? (
+                $("#medal").css("animation", "fadeDown ease-in-out 0.5s"), 
+                $("#medal").css("animation-iteration-count", "1"), 
+                $("#sld_1").attr("src", "images/RewardsPage/1.png"), 
+                $("#sld_2").attr("src", "images/RewardsPage/2.png"), 
+                $("#sld_3").attr("src", "images/RewardsPage/1.png"), 
+                $("#sld_4").attr("src", "images/RewardsPage/2.png"),
+                $('#section4 .image_block img:nth-child(n+2)').css('animation','tada ease-in-out 2s'),
+                $('#section4 .image_block img:nth-child(n+2)').css('animation-iteration-count','infinite'),
+
+
+
+                $(".pins").css("animation", ""), 
+                $(".pins").css("animation-iteration-count", ""), 
+                $('.pinss').css('animation',''),
+                $('.pinss').css('animation-iteration-count',''),
+                $('.cloud').css('animation',''),
+                $('.cloud').css('animation-iteration-count',''),
+                $('.cycle').css('animation',''),
+                $('.cycle').css('animation-iteration-count',''),
+
+
+                $('#head_top').css('animation',''),
+                $('#head_top').css('animation-iteration-count',''),
+                $('#head_bottom').css('animation',''),
+                $('#head_bottom').css('animation-iteration-count',''),
+                $('#head_left').css('animation',''),
+                $('#head_left').css('animation-iteration-count',''),
+                $('#head_right').css('animation',''),
+                $('#head_right').css('animation-iteration-count','')
+
+
+
+
+
+
+            ) : (
+                $(".pins").css("animation", ""), 
+                $(".pins").css("animation-iteration-count", ""), 
+                $("#medal").css("animation", ""), 
+                $("#medal").css("animation-iteration-count", "")
+            )
         }
     }), $("#fp-nav").css("margin-top", "0px"), $("#fp-nav").css("width", "19px"), $("#fp-nav").css("height", "19px");
     var a = 1;
